7210f657a1760e53df5580af4068da6d45c6f6e9
Martin Betak <mbetak@redhat.com>
2014-10-07 19:25:27 +0200

    Revert "Add User entity"
    This reverts commit f2cce518b92795c71c735e10ea9ab0e43cf4e188.

--- src/main/java/cz/muni/fi/pa165/methanolmanager/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package cz.muni.fi.pa165.methanolmanager.repository;
-
-import cz.muni.fi.pa165.methanolmanager.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
