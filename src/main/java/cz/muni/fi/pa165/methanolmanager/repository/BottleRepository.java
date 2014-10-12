ad1af944c43594eb3134ecda2a751c476dd5367f
Martin Betak <mbetak@redhat.com>
2014-10-12 20:05:18 +0200

    Add toxicity reporting & add setup to BottleTest

--- src/main/java/cz/muni/fi/pa165/methanolmanager/repository/BottleRepository.java
+++ src/main/java/cz/muni/fi/pa165/methanolmanager/repository/BottleRepository.java
@@ -1,6 +1,9 @@
 package cz.muni.fi.pa165.methanolmanager.repository;
 
 import cz.muni.fi.pa165.methanolmanager.domain.Bottle;
+import cz.muni.fi.pa165.methanolmanager.domain.Make;
+import cz.muni.fi.pa165.methanolmanager.domain.Producer;
+import cz.muni.fi.pa165.methanolmanager.domain.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,8 +12,19 @@ import java.util.List;
 
 public interface BottleRepository extends JpaRepository<Bottle, Integer> {
     Bottle findByName(String name);
-    List<Bottle> findByToxicTrue();
+
+    @Query("select b from Bottle b where b.toxic = true")
+    List<Bottle> findAllToxic();
 
     @Query("select b from Bottle b where b.stampDate is not null")
     List<Bottle> findAllStamped();
+
+    @Query("select count(b) from Bottle b where b.toxic = true and b.make.producer = ?1")
+    long countToxicByProducer(Producer producer);
+
+    @Query("select count(b) from Bottle b where b.toxic = true and b.make = ?1")
+    long countToxicByMake(Make make);
+
+    @Query("select count(b) from Bottle b where b.toxic = true and b.store = ?1")
+    long countToxicByStore(Store store);
 }
--- src/test/java/cz/muni/fi/pa165/methanolmanager/BottleTest.java
+++ src/test/java/cz/muni/fi/pa165/methanolmanager/BottleTest.java
@@ -1,19 +1,30 @@
 package cz.muni.fi.pa165.methanolmanager;
 
-import cz.muni.fi.pa165.methanolmanager.domain.Bottle;
-import cz.muni.fi.pa165.methanolmanager.repository.BottleRepository;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+
+import java.util.Arrays;
+import java.util.Date;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import cz.muni.fi.pa165.methanolmanager.domain.Store;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.inject.Inject;
-import javax.transaction.Transactional;
-import java.util.Arrays;
-import java.util.Date;
-
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
+import cz.muni.fi.pa165.methanolmanager.domain.Bottle;
+import cz.muni.fi.pa165.methanolmanager.domain.Make;
+import cz.muni.fi.pa165.methanolmanager.domain.Producer;
+import cz.muni.fi.pa165.methanolmanager.repository.BottleRepository;
+import cz.muni.fi.pa165.methanolmanager.repository.MakeRepository;
+import cz.muni.fi.pa165.methanolmanager.repository.ProducerRepository;
+import cz.muni.fi.pa165.methanolmanager.repository.StoreRepository;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
@@ -22,6 +33,66 @@ public class BottleTest {
 
     @Inject
     BottleRepository bottleRepository;
+
+    @Inject
+    MakeRepository makeRepository;
+
+    @Inject
+    ProducerRepository producerRepository;
+
+    @Inject
+    StoreRepository storeRepository;
+
+    private Producer bozkovProducer;
+    private Producer spisProducer;
+
+    private Make bozkov;
+    private Make spis;
+
+    private Store brnenka;
+    private Store tesco;
+    
+    @Before
+    public void setup() {
+        setupProducers();
+        setupMakes();
+        setupStores();
+    }
+
+    private void setupProducers() {
+        bozkovProducer = new Producer();
+        bozkovProducer.setName("Bozkov s.r.o");
+
+        spisProducer = new Producer();
+        spisProducer.setName("Spis s.r.o");
+
+        producerRepository.save(bozkovProducer);
+        producerRepository.save(spisProducer);
+    }
+
+    private void setupMakes() {
+        bozkov = new Make();
+        bozkov.setName("Bozkov");
+        bozkov.setProducer(bozkovProducer);
+
+        spis = new Make();
+        spis.setName("Spis");
+        spis.setProducer(spisProducer);
+
+        makeRepository.save(bozkov);
+        makeRepository.save(spis);
+    }
+
+    private void setupStores() {
+        brnenka = new Store();
+        brnenka.setName("Brnenka");
+
+        tesco = new Store();
+        tesco.setName("Tesco");
+
+        storeRepository.save(brnenka);
+        storeRepository.save(tesco);
+    }
 
     @Test
     public void testCreateBottle() {
@@ -69,7 +140,7 @@ public class BottleTest {
         bottleRepository.save(bottle);
         bottleRepository.save(bottle2);
 
-        assertThat(bottleRepository.findByToxicTrue(), is(Arrays.asList(bottle2)));
+        assertThat(bottleRepository.findAllToxic(), is(Arrays.asList(bottle2)));
     }
 
     @Test
@@ -92,4 +163,41 @@ public class BottleTest {
         assertThat(bottleRepository.findAllStamped().size(), equalTo(2));
     }
 
+    @Test
+    public void testCountToxic() {
+        Bottle bottle = new Bottle();
+        bottle.setName("Vodka");
+        bottle.setMake(bozkov);
+        bottle.setStore(brnenka);
+
+        Bottle bottle2 = new Bottle();
+        bottle2.setName("Tuzemak");
+        bottle2.setToxic(true);
+        bottle2.setMake(bozkov);
+        bottle2.setStore(brnenka);
+
+        Bottle bottle3 = new Bottle();
+        bottle3.setName("Slivka");
+        bottle3.setToxic(true);
+        bottle3.setMake(spis);
+        bottle3.setStore(tesco);
+
+        Bottle bottle4 = new Bottle();
+        bottle4.setName("Ceresna");
+        bottle4.setMake(spis);
+        bottle3.setStore(tesco);
+
+        bottleRepository.save(bottle);
+        bottleRepository.save(bottle2);
+        bottleRepository.save(bottle3);
+
+        assertThat(bottleRepository.countToxicByMake(bozkov), is(equalTo(1l)));
+        assertThat(bottleRepository.countToxicByMake(spis), is(equalTo(1l)));
+
+        assertThat(bottleRepository.countToxicByProducer(bozkovProducer), is(equalTo(1l)));
+        assertThat(bottleRepository.countToxicByProducer(spisProducer), is(equalTo(1l)));
+
+        assertThat(bottleRepository.countToxicByStore(brnenka), is(equalTo(1l)));
+        assertThat(bottleRepository.countToxicByStore(tesco), is(equalTo(1l)));
+    }
 }
