9f1bb44e447ac4ddb2ca9800d99b618dc3d8dc00
vomyy <pavomac@gmail.com>
2014-10-12 22:31:21 +0200

    Added store create and delete test

--- src/main/java/cz/muni/fi/pa165/methanolmanager/repository/StoreRepository.java
+++ src/main/java/cz/muni/fi/pa165/methanolmanager/repository/StoreRepository.java
@@ -7,4 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by pavel on 12.10.14.
  */
 public interface StoreRepository extends JpaRepository<Store, Integer> {
+    Store findByName(String name);
 }
--- src/test/java/cz/muni/fi/pa165/methanolmanager/StoreTest.java
+++ src/test/java/cz/muni/fi/pa165/methanolmanager/StoreTest.java
@@ -1,12 +1,16 @@
 package cz.muni.fi.pa165.methanolmanager;
 
-import cz.muni.fi.pa165.methanolmanager.repository.BottleRepository;
+import cz.muni.fi.pa165.methanolmanager.domain.Store;
+import cz.muni.fi.pa165.methanolmanager.repository.StoreRepository;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
 /**
  * Created by pavel on 12.10.14.
@@ -17,5 +21,50 @@ import javax.transaction.Transactional;
 public class StoreTest {
 
     @Inject
-    BottleRepository bottleRepository;
+    StoreRepository storeRepository;
+
+    @Test
+    public void testCreateStore() {
+        Store storeNamesti = new Store();
+        storeNamesti.setName("Na namesti");
+
+        Store storeHodiny = new Store();
+        storeHodiny.setName("Pod hodinami");
+
+        Store storeTesco = new Store();
+        storeTesco.setName("Tesco");
+
+        storeRepository.save(storeNamesti);
+        storeRepository.save(storeHodiny);
+        storeRepository.save(storeTesco);
+
+        assertThat(storeRepository.findAll().size(), equalTo(3));
+        assertThat(storeRepository.findByName("Tesco"), equalTo(storeTesco));
+    }
+
+    @Test
+    public void testDeleteStore() {
+        Store storeNamesti = new Store();
+        storeNamesti.setName("Na namesti");
+
+        Store storeHodiny = new Store();
+        storeHodiny.setName("Pod hodinami");
+
+        Store storeTesco = new Store();
+        storeTesco.setName("Tesco");
+
+        storeRepository.save(storeNamesti);
+        storeRepository.save(storeHodiny);
+        storeRepository.save(storeTesco);
+
+        assertThat(storeRepository.findAll().size(), equalTo(3));
+        assertThat(storeRepository.findByName("Tesco"), equalTo(storeTesco));
+
+        Store deleteThisStore = storeRepository.findByName("Tesco");
+
+        storeRepository.delete(deleteThisStore);
+
+        assertThat(storeRepository.findAll().size(), equalTo(2));
+        assertThat(storeRepository.findByName("Tesco"), nullValue());
+    }
 }
