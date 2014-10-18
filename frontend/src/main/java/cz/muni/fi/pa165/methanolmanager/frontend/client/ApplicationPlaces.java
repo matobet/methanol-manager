ff54d2f42b92f566cb4c06ac488f8b385755e273
Martin Betak <mbetak@redhat.com>
2014-10-18 23:57:05 +0200

    Add PWT dependency that doesn't fail!

--- frontend/src/main/java/cz/muni/fi/pa165/methanolmanager/frontend/client/ApplicationPlaces.java
+++
@@ -1,5 +0,0 @@
-package cz.muni.fi.pa165.methanolmanager.frontend.client;
-
-public interface ApplicationPlaces {
-    String MAIN_PLACE = "main";
-}
--- frontend/src/main/java/cz/muni/fi/pa165/methanolmanager/frontend/client/GwtApp.java
+++ frontend/src/main/java/cz/muni/fi/pa165/methanolmanager/frontend/client/GwtApp.java
@@ -1,12 +1,13 @@
-package cz.muni.fi.pa165.methanolmanager.frontend;
+package cz.muni.fi.pa165.methanolmanager.frontend.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.RootPanel;
+import fr.putnami.pwt.core.widget.client.OutputStaticText;
 
 public class GwtApp implements EntryPoint {
     @Override
     public void onModuleLoad() {
-        RootPanel.get().add(new Label("Hello world!!!!"));
+        RootPanel.get().add(new OutputStaticText("Hello Putnami"));
     }
 }
