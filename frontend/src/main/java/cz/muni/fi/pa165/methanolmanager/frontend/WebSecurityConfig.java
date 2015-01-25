513be6b87acfc53805c17920588e8fe2d6e12b13
Martin Betak <matobet@gmail.com>
2015-01-25 02:40:05 +0100

    frontend: Fix security patterns

--- frontend/src/main/java/cz/muni/fi/pa165/methanolmanager/frontend/WebSecurityConfig.java
+++ frontend/src/main/java/cz/muni/fi/pa165/methanolmanager/frontend/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package cz.muni.fi.pa165.methanolmanager.frontend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -19,14 +20,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService userDetailsService;
 
-   /* @Override
+    @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers().permitAll()
-                .anyRequest().fullyAuthenticated();
+                .antMatchers("/api/**").authenticated()
+                .antMatchers("/**").permitAll();
         http.httpBasic();
         http.csrf().disable();
-    }*/
+    }
 
     @Override
     public void configure(AuthenticationManagerBuilder auth) throws Exception {
